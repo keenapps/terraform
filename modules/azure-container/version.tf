6295288b8f139aa7c519b4e53190ce82d11948aa
Outismcanon <stefan.fo@mailbox.org>
2026-02-07 00:25:39 +0100

    fixed azure storage container module version

--- modules/azure-container/version.tf
+++ modules/azure-container/version.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.14.0" # Required Terraform-Binary-Version
+  required_version = ">= 1.10.0" # Required Terraform-Binary-Version
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 4.1"   # Allow this Provider Version only ( avoids Breaking Changes)
+      version = ">= 4.1, < 5.0"   # Allow this Provider Versions only ( avoids Breaking Changes)
     }
   }
 }
