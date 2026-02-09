28ce34a05d7831522c6fff4a7632151323a8c2ef
Outismcanon <stefan.fo@mailbox.org>
2026-02-09 17:32:22 +0100

    fixed a typo

--- projects/00-azure-remote-state-demo/provider.tf
+++ projects/00-azure-remote-state-demo/provider.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
   resource_provider_registrations = "none" # Disables automatic registration of Azure Resource Providers
   # (useful in restricted environments; you must pre-register providers manually)
   use_oidc = true # Use Azure AD Workload Identity (OIDC) instead of access keys (not further configured in this demo)
-
+}
 terraform {
   required_providers {
     azurerm = { source = "hashicorp/azurerm", version = "~> 4.0" }
