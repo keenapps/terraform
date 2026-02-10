156e0b2f41825371366434bedd0b2329c8baa5b5
Outismcanon <stefan.fo@mailbox.org>
2026-02-10 23:13:40 +0100

    force OIDC

--- projects/02-azure-cloudeteer-launchpad-demo/provider.tf
+++ projects/02-azure-cloudeteer-launchpad-demo/provider.tf
@@ -3,4 +3,5 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id                 = var.subscription_id
   tenant_id                       = var.tenant_id
+  use_oidc                        = true
 }
