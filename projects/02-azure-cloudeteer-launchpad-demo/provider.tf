f379f7f557734e7a6ca5303b58123ffe2ff8a162
Outismcanon <stefan.fo@mailbox.org>
2026-02-10 23:39:54 +0100

    fixing workflow error

--- projects/02-azure-cloudeteer-launchpad-demo/provider.tf
+++ projects/02-azure-cloudeteer-launchpad-demo/provider.tf
@@ -4,4 +4,5 @@ provider "azurerm" {
   subscription_id                 = var.subscription_id
   tenant_id                       = var.tenant_id
   use_oidc                        = true
+  use_cli                         = false
 }
