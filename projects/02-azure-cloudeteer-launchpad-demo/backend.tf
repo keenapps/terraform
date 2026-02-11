1bbd1dae529767e2814f31b617537ac44f7f5d87
Outismcanon <stefan.fo@mailbox.org>
2026-02-11 15:16:20 +0100

    remote backend enabled

--- projects/02-azure-cloudeteer-launchpad-demo/backend.tf
+++ projects/02-azure-cloudeteer-launchpad-demo/backend.tf
@@ -1,8 +1,8 @@
-/* terraform {
+terraform {
   backend "azurerm" {
     resource_group_name  = "rg-launchpad"
-    storage_account_name = "stlaunchpadz3c"
+    storage_account_name = "stlaunchpad8uf"
     container_name       = "tfstate"
     key                  = "launchpad.tfstate"
   }
-} */
+}
