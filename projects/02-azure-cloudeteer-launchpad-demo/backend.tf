89828224db6bff4a8e54910d5a6b51f40b677de8
Outismcanon <stefan.fo@mailbox.org>
2026-02-11 11:30:26 +0100

    backend configured

---
+++ projects/02-azure-cloudeteer-launchpad-demo/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-launchpad"
+    storage_account_name = "stlaunchpadrtm"
+    container_name       = "tfstate"
+    key                  = "launchpad.tfstate"
+  }
+}
