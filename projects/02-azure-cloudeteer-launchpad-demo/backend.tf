69c770a373c73e2efec00bfe140ab9c203915ba6
Outismcanon <stefan.fo@mailbox.org>
2026-02-11 23:32:05 +0100

    implemented use_azuread_auth

--- projects/02-azure-cloudeteer-launchpad-demo/backend.tf
+++ projects/02-azure-cloudeteer-launchpad-demo/backend.tf
@@ -1,8 +1,11 @@
+# Block comment file before 1st terraform init /apply
+# Change storage_account_name after 1st apply to actual string
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-launchpad"
-    storage_account_name = "stlaunchpadoaa"
+    storage_account_name = "stlaunchpadebv"  #change to actual storage account"
     container_name       = "tfstate"
     key                  = "launchpad.tfstate"
+    use_azuread_auth     = true
   }
 }
