c2f3b785751f3851c1a8d3e076481e1150b1aed9
Outismcanon <stefan.fo@mailbox.org>
2026-02-11 11:48:53 +0100

    disabled backend.tf for test purpose

--- projects/02-azure-cloudeteer-launchpad-demo/backend.tf
+++ projects/02-azure-cloudeteer-launchpad-demo/backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/* terraform {
   backend "azurerm" {
     resource_group_name  = "rg-launchpad"
     storage_account_name = "stlaunchpadrtm"
     container_name       = "tfstate"
     key                  = "launchpad.tfstate"
   }
-}
+} */
