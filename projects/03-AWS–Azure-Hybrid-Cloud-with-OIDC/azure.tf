378dbb5b75ddc734c1a54b2a008145f7824650ff
Outismcanon <stefan.fo@mailbox.org>
2026-03-03 17:40:13 +0100

    deleted unnessesary resourced

--- projects/03-AWS–Azure-Hybrid-Cloud-with-OIDC/azure.tf
+++ projects/03-AWS–Azure-Hybrid-Cloud-with-OIDC/azure.tf
@@ -1,108 +1,55 @@
 data "azurerm_subscription" "current" {}
+data "azurerm_client_config" "current" {}
+data "azurerm_storage_account" "demo2" {
+  name                = "stlaunchpad5id"
+  resource_group_name = "rg-launchpad"
+}
 
 # Resource Block
 resource "azurerm_resource_group" "this" {
   location = "Switzerland North"
-  name     = "rg-launchpad"
+  name     = "hybrid-aws-azure-demo"
 }
-
-# Network Block
-resource "azurerm_virtual_network" "this" {
-  name                = "vnet-launchpad"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  address_space       = ["192.168.42.0/27"]
-}
-
-# Subnet Block
-resource "azurerm_subnet" "this" {
-  name                 = "snet-launchpad"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["192.168.42.0/28"]
-}
-
-resource "azurerm_user_assigned_identity" "this" {
-  name                = "id-launchpad"
+# UAMI + Federated for GitHub OIDC (Backend)
+resource "azurerm_user_assigned_identity" "backend" {
+  name                = "id-hybrid-aws-azure-demo"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
 
-resource "azurerm_federated_identity_credential" "this" {
-  for_each = toset(["prod-azure", "prod-azure-plan"])
+resource "azurerm_federated_identity_credential" "backend" {
+  for_each = toset(["hybrid-aws-azure-demo"])
 
   name                = each.key
   audience            = ["api://AzureADTokenExchange"]
   issuer              = "https://token.actions.githubusercontent.com"
-  parent_id           = azurerm_user_assigned_identity.this.id
-  resource_group_name = azurerm_user_assigned_identity.this.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.backend.id
+  resource_group_name = azurerm_user_assigned_identity.backend.resource_group_name
   subject             = "repo:keenapps/${var.runner_github_repo}:environment:${each.key}"
 }
 
-resource "azurerm_role_assignment" "subscription_owner" {
-  principal_id         = azurerm_user_assigned_identity.this.principal_id
-  role_definition_name = "Owner"
-  scope                = data.azurerm_subscription.current.id
+resource "azurerm_role_assignment" "storage_data_hybrid" {
+  principal_id         = azurerm_user_assigned_identity.backend.principal_id
+  role_definition_name = "Storage Blob Data Owner"
+  scope                = data.azurerm_storage_account.demo2.id
 }
 
-resource "random_string" "this" {
+resource "random_string" "backend" {
   length  = 3
   special = false
   upper   = false
 }
 
-# Storage Account
-resource "azurerm_storage_account" "this" {
-  name                = "stlaunchpad${random_string.this.result}"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
-
-  allow_nested_items_to_be_public   = false
-  default_to_oauth_authentication   = true
-  infrastructure_encryption_enabled = true
-  public_network_access_enabled     = true
-  shared_access_key_enabled         = false
-
-  blob_properties {
-    versioning_enabled = true
-  }
+# Generate SSH Key Pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-# Storage Container
-resource "azurerm_storage_container" "this" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.this.id
-  container_access_type = "private"
-}
-
-resource "azurerm_role_assignment" "this" {
-  principal_id         = azurerm_user_assigned_identity.this.principal_id
-  scope                = azurerm_storage_account.this.id
-  role_definition_name = "Storage Blob Data Owner"
-}
-
-# Resource lock
-resource "azurerm_management_lock" "this" {
-  name       = "storage-account-lock"
-  lock_level = "CanNotDelete"
-  scope      = azurerm_storage_account.this.id
-}
-
-# DNS Zone
-resource "azurerm_private_dns_zone" "blob" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
-  name                  = "vnet-link-blob"
-  resource_group_name   = azurerm_resource_group.this.name
-  private_dns_zone_name = azurerm_private_dns_zone.blob.name
-  virtual_network_id    = azurerm_virtual_network.this.id
-  registration_enabled  = false
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/id_rsa"
+  file_permission = "0600"
 }
 
 # RG for Compute
@@ -115,14 +62,14 @@ resource "azurerm_resource_group" "compute" {
 resource "azurerm_virtual_network" "compute" {
   name                = "terraform-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 # Subnet - Network Segment Inside the VNet
 resource "azurerm_subnet" "compute_subnet" {
   name                 = "compute_subnet"
-  resource_group_name  = azurerm_resource_group.compute.name
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.compute.name
   address_prefixes     = ["10.0.2.0/24"]
 
@@ -132,8 +79,8 @@ resource "azurerm_subnet" "compute_subnet" {
 # Network Interface (NIC) - VM Network Adapter
 resource "azurerm_network_interface" "compute-nic" {
   name                = "compute-nic"
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
   ip_configuration {
     name                          = "internal"
@@ -145,12 +92,17 @@ resource "azurerm_network_interface" "compute-nic" {
 # Linux Virtual Machine - Compute Instance
 resource "azurerm_linux_virtual_machine" "compute_vm" {
   name                = "ec2-machine"
-  resource_group_name = azurerm_resource_group.compute.name
-  location            = azurerm_resource_group.compute.location
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
   size                = "Standard_D2s_v3"
 
   admin_username                  = "adminuser"
   disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
 
   network_interface_ids = [
     azurerm_network_interface.compute-nic.id,
@@ -174,12 +126,4 @@ resource "azurerm_linux_virtual_machine" "compute_vm" {
 
 output "resource_group_name" {
   value = azurerm_resource_group.this.name
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.this.name
-}
-
-output "container_name" {
-  value = azurerm_storage_container.this.name
 }
